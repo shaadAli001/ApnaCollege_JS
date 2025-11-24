05ce8f644653bd1b141d65bbf2a1270602387313
shaadali001 <alishaad908@gmail.com>
2025-11-24 22:07:53 +0530

    Array complete

---
+++ Arrays/script.js
@@ -0,0 +1,47 @@
+// let marks = [98, 23, 45, 12, 67];
+// console.log(marks.length);
+// console.log(typeof (marks));
+// marks[3]=44;
+// console.log(marks);
+
+
+// let names = ['shiva', 'Hari', 'Salam', 'Kiran', 'Albert', 'Luffy'];
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+// for (const element of names) {
+//     console.log(element);
+
+// }
+
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+// let avg;
+// for (let i = 0; i < marks.length; i++) {
+//     sum += marks[i];
+//     avg = sum / marks.length;
+// }
+// console.log(avg);
+
+// let arr = [250, 645, 300, 900, 50];
+// let new_arr = [];
+// for (let i = 0; i < arr.length; i++) {
+//     new_arr.push(arr[i] - (arr[i] * 0.1));
+// }
+// console.log(new_arr);
+
+// console.log(arr.concat(names));
+// let a=arr.slice(1,arr.length);
+// let a2=arr.splice(0,0,333);
+// console.log(a);
+// console.log(arr);
+
+// let str = ['Bloomberg', 'Microsoft', 'Uber', 'Google', 'IBM', 'Netflix']
+// str.pop();
+// console.log(str);
+
+// str.splice(2,1,'Ola');
+// console.log(str);
+
+// str.push('Amazon');
+// console.log(str);
