d6eb5aa1ca7c05dddde51abd6b88e58ac06f0028
shaadAli001 <alishaad908@gmail.com>
2025-11-24 10:38:54 +0530

    Chapter-1 complete

---
+++ Chapter-1_Var_and_data/script.js
@@ -0,0 +1,19 @@
+let name = "Shaad";
+let Age = 22;
+let degree = "B.Tech";
+console.log(`${name}\n${Age}\n${degree}`);
+
+const pi = 22 / 7;
+let r = 8;
+console.log(pi * r * r);
+
+let student = {
+  Name: "Shaad ali",
+  Age: 22,
+  CGPA: 8.71,
+  isPass: true,
+};
+console.log(student.Name.toUpperCase());
+// console.log(typeof(student));
+let{CGPA}=student;
+console.log(CGPA);
