04eb040a12d79e95838ccbb8a7a2e1dfa2486fcc
shaadAli001 <alishaad908@gmail.com>
2025-11-24 16:11:35 +0530

    Loops and Strings complete

---
+++ Loops_and_String/script.js
@@ -0,0 +1,61 @@
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   console.log("i = ", i);
+// }
+// let size = 0;
+// let str = "HelloWorld";
+// for (const i of str) {
+//   /*Used in String*/
+//   console.log("i=", i);
+//   size++;
+// }
+// console.log("size =", size);
+
+// let student = {
+//   name: "Shaad Ali",
+//   age: 20,
+//   cgpa: 8.71,
+//   ispass: true,
+// };
+// for (const key in student) {
+//   console.log("Key=", key, "Value=", student[key]);
+// }
+
+// Q1
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// Q2
+// let userGuess = prompt("Enter the number you gussed");
+// let winningNumber = 25;
+
+// while (userGuess != winningNumber) {
+//   userGuess = prompt("Enter the number you gussed");
+// }
+// console.log("You won");
+
+// Strings
+// let str = "Shaad ali";
+// console.log(str.toUpperCase());
+
+// let student = {
+//   name: "Shaad Ali",
+//   age: 22,
+//   cgpa: 8.71,
+// };
+// console.log(`Name:${student.name.}\nAge:${student.age}\nCGPA:${student.cgpa}`);
+
+// let str1 = "hello";
+// let str2 = "world";
+// console.log(str1.concat(str2));
+// console.log(str1.replace('hello','hello world'));
+// console.log(str1.charAt(2));
+
+// console.log(str.slice(0,5));
+
+// let str = prompt("Enter Your Full Name") || "Ramesh";
+// let newstr ='@'+str.concat(str.length);
+// console.log(newstr);
