788a227fb70722b95d1c10b3a3f89662dd614437
shaadAli001 <alishaad908@gmail.com>
2025-11-25 14:47:07 +0530

    DOM completed

---
+++ DOM/script.js
@@ -0,0 +1,19 @@
+// let heading = document.getElementById("heading");
+// console.log(heading);
+
+// let para = document.getElementsByClassName("para");
+
+// let btn=document.getElementsByTagName("button")
+// console.dir(btn)
+
+// let heading=document.querySelector("#heading")
+// console.log(heading.innerText);
+
+// document.querySelector('div').innerHTML='<div>Lorem</div>
+
+let heading = document.querySelector("h2");
+console.dir(heading);
+heading.innerText += " from APNA College Students";
+
+let para = document.querySelectorAll(".para");
+console.dir(para)
