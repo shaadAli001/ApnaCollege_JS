dc1250e56ef4e890f49804c18e0957c3dc6692e8
shaadAli001 <alishaad908@gmail.com>
2025-11-25 17:09:44 +0530

    pending

--- DOM/script.js
+++ DOM/script.js
@@ -1,19 +1,41 @@
-// let heading = document.getElementById("heading");
-// console.log(heading);
+// // let heading = document.getElementById("heading");
+// // console.log(heading);
 
-// let para = document.getElementsByClassName("para");
+// // let para = document.getElementsByClassName("para");
 
-// let btn=document.getElementsByTagName("button")
-// console.dir(btn)
+// // let btn=document.getElementsByTagName("button")
+// // console.dir(btn)
 
-// let heading=document.querySelector("#heading")
-// console.log(heading.innerText);
+// // let heading=document.querySelector("#heading")
+// // console.log(heading.innerText);
 
-// document.querySelector('div').innerHTML='<div>Lorem</div>
+// // document.querySelector('div').innerHTML='<div>Lorem</div>
 
-let heading = document.querySelector("h2");
-console.dir(heading);
-heading.innerText += " from APNA College Students";
+// // let heading = document.querySelector("h2");
+// // console.dir(heading);
+// // heading.innerText += " from APNA College Students";
 
-let para = document.querySelectorAll(".para");
-console.dir(para)
+// // let para = document.querySelector(".para");
+// // console.dir(para);
+
+// // Add Style
+// // let para = document.querySelector("p");
+// // para.style.fontSize = "2rem";
+// // para.innerText = "Hello";
+// // console.log(para.setAttribute("class", "newclass"));
+
+// // Insert Element
+// let div = document.querySelector("div");
+// let btn = document.createElement("button");
+// btn.innerText = "click Me";
+// div.append(btn);
+
+let btn = document.createElement("button");
+let body = document.querySelector("body");
+
+btn.innerText = "Click Me";
+btn.style.backgroundColor = "red";
+btn.style.color = "white";
+
+body.append(btn);
+
