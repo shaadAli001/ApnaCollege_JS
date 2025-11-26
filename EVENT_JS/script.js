0ff2fc9810c75a98dfb76b4775425d83f1015098
shaadali001 <alishaad908@gmail.com>
2025-11-26 23:04:21 +0530

    events complete

--- EVENT_JS/script.js
+++ EVENT_JS/script.js
@@ -1,12 +1,28 @@
-let btn = document.querySelector("button");
-btn.addEventListener("click", () => {
-  console.log("hello");
-});
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", () => {
+//   console.log("hello");
+// });
 
-let div = document.querySelector("div");
-div.addEventListener("click", (e) => {
-  console.log(e.target);
-  console.log(e.type);
+// let div = document.querySelector("div");
+// div.addEventListener("click", (e) => {
+//   console.log(e.target);
+//   console.log(e.type);
 
-  console.log("you hovered");
-});
+//   console.log("you hovered");
+// });
+
+let btn = document.querySelector("#btn");
+let body = document.querySelector('body');
+let theme = 'light';
+
+btn.addEventListener('click', () => {
+  if (theme === 'light') {
+    theme = 'dark';
+    body.classList.add('dark')
+    body.classList.remove('light')
+  } else {
+    theme = 'light';
+    body.classList.add('light')
+    body.classList.remove('dark')
+  }
+})
