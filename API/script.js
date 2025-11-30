bdd862e328042d44b50c37b68ef9c875b2662b0a
shaadali001 <alishaad908@gmail.com>
2025-11-30 21:16:43 +0530

    API complete

--- API/script.js
+++ API/script.js
@@ -1,11 +1,26 @@
 const URL = 'https://api.thecatapi.com/v1/images/search';
+let btn = document.querySelector("#btn");
+let image = document.getElementById("img");
 
 const getImage = async () => {
     let response = await fetch(URL);
     const data = await response.json();
-    document.getElementById('img').src = data[0].url;
-    console.log(data[0].url);
-
+    image.src = data[0].url;
 }
-getImage()
+// Using Promises
+// function getImage2() {
+//     fetch(URL)
+//         .then((response) => {
+//             return response.json();
+//         }).then((data) => {
+//             console.log(data[0].url);
+
+//         })
+// }
+
+btn.addEventListener('click', () => {
+    image.style.height = '200px';
+    image.style.width = '200px';
+    getImage();
+});
 
